ce7e72120cda3adceb0215f1b7c3e9972df428d0
RobertoGallego <groberto@student.42.fr>
2020-01-08 14:22:32 +0100

    ok1

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,5 @@
-const { ApolloServer, PubSub } = require('apollo-server');
-// const { ApolloServer, PubSub } = require('apollo-server-express');
+// const { ApolloServer, PubSub } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server-express');
 
 const mongoose = require('mongoose');
 
@@ -12,6 +12,7 @@ const express = require('express');
 const pubsub = new PubSub();
 
 const port = process.env.port || 5000;
+const path = "/graphql";
 
 const server = new ApolloServer({
   typeDefs,
@@ -19,23 +20,41 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub })
 });
 
-// const app = express();
+const app = express();
 
 // Specify port and path for GraphQL endpoint
 // const port = process.env.GRAPHQL_LISTEN_PORT || 5000;
 // const path = "/graphql";
+const connection = mongoose
+  .connect(MONGODB, { useNewUrlParser: true });
+  // .then(() => {
+  //   console.log('MongoDB Connected');
+  //   return server.listen({ port});
+  // })
+  // .then((res) => {
+  //   console.log(`Server running at ${res.url}`);
+  // })
+  // .catch(err => {
+  //   console.error(err)
+  // })
 
-// server.applyMiddleware({ app });
-
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
-  .then(() => {
-    console.log('MongoDB Connected');
-    return server.listen({ port});
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  })
+  connection
+  .then(db => db)
   .catch(err => {
-    console.error(err)
-  })
+      console.log(err);
+  });
+
+
+server.applyMiddleware({ app, path: '/', cors: true, onHealthCheck: () => new Promise((resolve, reject) => {
+  if (mongoose.connection.readyState > 0) {
+      resolve();
+  } else {
+      reject();
+  }
+}),
+});
+
+app.listen({ port, path }, () => {
+  console.log(`🚀  Server listening on port http://localhost:${port}${path}`);
+  // console.log(`🚀  Health checks available at ${process.env.HEALTH_ENDPOINT}`);
+});
