6bf45539cf89fd8abb0af1f6639904b999fdd7cc
RobertoGallego <groberto@student.42.fr>
2020-01-08 15:53:36 +0100

    ok

--- api/src/graphql/typeDefs.js
+++ api/src/graphql/typeDefs.js
@@ -38,6 +38,7 @@ module.exports = gql`
   type Query {
     getPosts: [Post]
     getPost(postId: ID!): Post
+    getUsers: [User]
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
