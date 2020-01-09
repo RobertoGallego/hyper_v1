941b83eef8811cd75888c8e965fdb7f84fd09547
RobertoGallego <groberto@student.42.fr>
2020-01-09 13:47:22 +0100

    babel ok

--- api/src/graphql/resolvers/index.js
+++ api/src/graphql/resolvers/index.js
@@ -3,20 +3,20 @@ const usersResolvers = require('./users');
 const commentsResolvers = require('./comments');
 
 module.exports = {
-  Post: {
-    likeCount: (parent) => parent.likes.length,
-    commentCount: (parent) => parent.comments.length
-  },
-  Query: {
-    ...postsResolvers.Query,
-    ...usersResolvers.Query
-  },
-  Mutation: {
-    ...usersResolvers.Mutation,
-    ...postsResolvers.Mutation,
-    ...commentsResolvers.Mutation
-  },
-  Subscription: {
-    ...postsResolvers.Subscription
-  }
+    Post: {
+        likeCount: parent => parent.likes.length,
+        commentCount: parent => parent.comments.length
+    },
+    Query: {
+        ...postsResolvers.Query,
+        ...usersResolvers.Query
+    },
+    Mutation: {
+        ...usersResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+    },
+    Subscription: {
+        ...postsResolvers.Subscription
+    }
 };
--- api/src/index.js
+++ api/src/index.js
@@ -7,17 +7,17 @@ const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
 
-const express = require('express');
+import express from 'express';
 
 const pubsub = new PubSub();
 
 const port = process.env.port || 5000;
-const path = "/graphql";
+const path = '/graphql';
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req, pubsub })
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req, pubsub })
 });
 
 const app = express();
@@ -25,36 +25,39 @@ const app = express();
 // Specify port and path for GraphQL endpoint
 // const port = process.env.GRAPHQL_LISTEN_PORT || 5000;
 // const path = "/graphql";
-const connection = mongoose
-  .connect(MONGODB, { useNewUrlParser: true });
-  // .then(() => {
-  //   console.log('MongoDB Connected');
-  //   return server.listen({ port});
-  // })
-  // .then((res) => {
-  //   console.log(`Server running at ${res.url}`);
-  // })
-  // .catch(err => {
-  //   console.error(err)
-  // })
+const connection = mongoose.connect(MONGODB, { useNewUrlParser: true });
+// .then(() => {
+//   console.log('MongoDB Connected');
+//   return server.listen({ port});
+// })
+// .then((res) => {
+//   console.log(`Server running at ${res.url}`);
+// })
+// .catch(err => {
+//   console.error(err)
+// })
 
-  connection
-  .then(db => db)
-  .catch(err => {
-      console.log(err);
-  });
+connection
+    .then(db => db)
+    .catch(err => {
+        console.log(err);
+    });
 
-
-server.applyMiddleware({ app, path: '/', cors: true, onHealthCheck: () => new Promise((resolve, reject) => {
-  if (mongoose.connection.readyState > 0) {
-      resolve();
-  } else {
-      reject();
-  }
-}),
+server.applyMiddleware({
+    app,
+    path: '/',
+    cors: true,
+    onHealthCheck: () =>
+        new Promise((resolve, reject) => {
+            if (mongoose.connection.readyState > 0) {
+                resolve();
+            } else {
+                reject();
+            }
+        })
 });
 
 app.listen({ port, path }, () => {
-  console.log(`🚀  Server listening on port http://localhost:${port}${path}`);
-  // console.log(`🚀  Health checks available at ${process.env.HEALTH_ENDPOINT}`);
+    console.log(`Server listening on port http://localhost:${port}${path}`);
+    // console.log(`🚀  Health checks available at ${process.env.HEALTH_ENDPOINT}`);
 });
