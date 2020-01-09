9e1842d602785c22446625a864e34038bb3abe47
RobertoGallego <groberto@student.42.fr>
2020-01-09 15:38:16 +0100

    moment fixed

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,5 @@
-// const { ApolloServer, PubSub } = require('apollo-server');
 const { ApolloServer, PubSub } = require('apollo-server-express');
-
 const mongoose = require('mongoose');
-
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config.js');
@@ -22,23 +19,12 @@ const server = new ApolloServer({
 
 const app = express();
 
-// Specify port and path for GraphQL endpoint
-// const port = process.env.GRAPHQL_LISTEN_PORT || 5000;
-// const path = "/graphql";
 const connection = mongoose.connect(MONGODB, { useNewUrlParser: true });
-// .then(() => {
-//   console.log('MongoDB Connected');
-//   return server.listen({ port});
-// })
-// .then((res) => {
-//   console.log(`Server running at ${res.url}`);
-// })
-// .catch(err => {
-//   console.error(err)
-// })
-
 connection
-    .then(db => db)
+    .then(db => {
+        db;
+        console.log('MongoDB Connected');
+    })
     .catch(err => {
         console.log(err);
     });
