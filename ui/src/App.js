c730c830900a9526ccb70fed53d281d1269ab2bf
Kyoya WATANABE <kwatanab@e1r4p15.42.fr>
2020-01-13 16:17:37 +0100

    Created the header

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
-
-// import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 import { AuthProvider } from './context/auth';
@@ -20,11 +17,9 @@ function App() {
     return (
         <AuthProvider>
             <Router>
-                <Container>
-                    <Route exact path='/' component={Home} />
-                    <AuthRoute exact path='/login' component={Login} />
-                    <AuthRoute exact path='/register' component={Register} />
-                </Container>
+                <Route exact path='/' component={Home} />
+                <AuthRoute exact path='/login' component={Login} />
+                <AuthRoute exact path='/register' component={Register} />
             </Router>
         </AuthProvider>
     );
--- ui/src/components/MenuBar.js
+++ ui/src/components/MenuBar.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react';
-import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-
+import { Menu } from 'semantic-ui-react';
 import { AuthContext } from '../context/auth';
+import './components.css';
+import logoTop from '../assets/images/logo-top.png';
 
 function MenuBar() {
   const { user, logout } = useContext(AuthContext);
@@ -14,23 +15,41 @@ function MenuBar() {
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   const menuBar = user ? (
-    <Menu pointing secondary size="massive" color="teal">
-      <Menu.Item name={user.username} active as={Link} to="/" />
+    // <Menu pointing secondary size="massive" color="teal">
+    //   <Menu.Item name={user.username} active as={Link} to="/" />
 
-      <Menu.Menu position="right">
-        <Menu.Item name="logout" onClick={logout} />
-      </Menu.Menu>
-    </Menu>
+    //   <Menu.Menu position="right">
+    //     <Menu.Item name="logout" as={Link} onClick={logout} />
+    //   </Menu.Menu>
+    // </Menu>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+      <a className="navbar-brand" href="/"><img src={logoTop} alt="Hypertube" height="75" width="140"/></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Profile</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Preferences</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/" onClick={logout}>Logout</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2 pr-sm-5" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+    </nav>
   ) : (
     <Menu pointing secondary size="massive" color="teal">
-      <Menu.Item
-        name="home"
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-        as={Link}
-        to="/"
-      />
-
       <Menu.Menu position="right">
         <Menu.Item
           name="login"
@@ -39,7 +58,7 @@ function MenuBar() {
           as={Link}
           to="/login"
         />
-        <p> --- </p>
+
         <Menu.Item
           name="register"
           active={activeItem === 'register'}
