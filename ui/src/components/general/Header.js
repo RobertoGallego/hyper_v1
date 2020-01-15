0b434c5441c83e976d70a03ae90b1880588081fa
Quentin FADENE <qfadene@e3r3p7.42.fr>
2020-01-15 14:10:52 +0100

    header

--- ui/src/components/general/Header.js
+++ ui/src/components/general/Header.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
 import Nav from './MenuBar'
-
-const Img = styled.img`
-    margin: 1rem 2rem;
-`;
 
 export default function Header() {
     return (
